8fb81f3281c87723f3ddd51ed70240f5656911f1
sanskar12k <sanskarmodanwal8@gmail.com>
2021-12-22 23:41:40 +0530

    Added DB

--- connection.php
+++ connection.php
@@ -1,9 +1,12 @@
 <?php
-
-    $DB_HOST = "localhost";
-    $DB_NAME = "aam";
-    $DB_USER = "root";
-    $DB_PASS = "";
+ $DB_HOST = "db4free.net:3306";
+ $DB_NAME = "sacaam";
+ $DB_USER = "sanskar12k";
+ $DB_PASS = "#atemis123";
+    // $DB_HOST = "localhost";
+    // $DB_NAME = "aam";
+    // $DB_USER = "root";
+    // $DB_PASS = "";
     /*
     $DB_HOST = "172.17.0.8";
     $database = "aam";
