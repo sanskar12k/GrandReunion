65316cbb5a7f7389c15afae8857d77d35d26d0ac
sanskar12k <sanskarmodanwal8@gmail.com>
2021-12-23 16:06:25 +0530

    Added db

--- connection.php
+++ connection.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
  $DB_HOST = "db4free.net:3306";
  $DB_NAME = "sacaam";
  $DB_USER = "sanskar12k";
@@ -8,19 +7,6 @@
     // $DB_NAME = "aam";
     // $DB_USER = "root";
     // $DB_PASS = "";
-=======
-
-    $DB_HOST = "localhost";
-    $DB_NAME = "aam";
-    $DB_USER = "root";
-    $DB_PASS = "";
-
-    // $DB_HOST = "db4free.net:3306";
-    // $DB_NAME = "grandreunion";
-    // $DB_USER = "umangsingla617";
-    // $DB_PASS = "12345678";
-
->>>>>>> 456218e298afb94e4ddece9c13beb054852b8117
     /*
     $DB_HOST = "172.17.0.8";
     $database = "aam";
